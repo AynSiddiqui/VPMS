e94428ee3f7810505d1427f4826ddaf54f25f21b
SID <bahetisid06@gmail.com>
2023-11-07 07:34:39 +0530

    cloud database added

--- DataBaseOperation.py
+++ DataBaseOperation.py
@@ -1,112 +1,115 @@
-import mysql.connector
-import json
+import psycopg2
 from datetime import datetime
 
 class DBOperation():
 
     def __init__(self):
-        file=open("./config.json","r")
-        datadic=json.loads(file.read())
-        file.close()
-        self.mydb = mysql.connector.connect(
-                    host="localhost",
-                    user="root",
-                    password="1111",  # Replace with your actual password
-                    database="vpms_py"
-                )
-        
-    def CreateTables(self):
-        cursor=self.mydb.cursor()
-        cursor.execute("DROP TABLE if exists admin")
-        cursor.execute("DROP TABLE if exists slots")
-        cursor.execute("DROP TABLE if exists vehicles")
-        cursor.execute("CREATE TABLE admin (id int(255) AUTO_INCREMENT PRIMARY KEY,username varchar(30),password varchar(30),created_at varchar(30))")
-        cursor.execute("CREATE TABLE slots (id int(255) AUTO_INCREMENT PRIMARY KEY,vehicle_id varchar(30),space_for int(25),is_empty int(25))")
-        cursor.execute("CREATE TABLE vehicles (id int(255) AUTO_INCREMENT PRIMARY KEY,name varchar(30),mobile varchar(30),entry_time varchar(30),exit_time varchar(30),is_exit varchar(30),vehicle_no varchar(30),vehicle_type varchar(30),created_at varchar(30),updated_at varchar(30))")
-        cursor.close()
+        connection_string = "postgresql://vpms_user:RUrfhs9jSEZz9uRM0HzW1iqWPx9XCIZF@dpg-cl4p7fs2np1s7383qb7g-a.singapore-postgres.render.com/vpms"
 
-    def InsertOneTimeData(self,space_for_two,space_for_four):
-        cursor=self.mydb.cursor()
+        try:
+            self.connection = psycopg2.connect(connection_string)
+            print("Connected to the DB")
+        except Exception as e:
+            print(f"Error: {e}")
+
+    def CreateTables(self):
+        # cursor = self.connection.cursor()
+
+        # # Drop tables if they exist
+        # cursor.execute("DROP TABLE IF EXISTS admin")
+        # cursor.execute("DROP TABLE IF EXISTS slots")
+        # cursor.execute("DROP TABLE IF EXISTS vehicles")
+
+        # # Create tables
+        # cursor.execute("CREATE TABLE admin (id SERIAL PRIMARY KEY, username VARCHAR(30), password VARCHAR(30), created_at VARCHAR(30))")
+        # cursor.execute("CREATE TABLE slots (id SERIAL PRIMARY KEY, vehicle_id VARCHAR(30), space_for INT, is_empty INT)")
+        # cursor.execute("CREATE TABLE vehicles (id SERIAL PRIMARY KEY, name VARCHAR(30), mobile VARCHAR(30), entry_time VARCHAR(30), exit_time VARCHAR(30), is_exit VARCHAR(30), vehicle_no VARCHAR(30), vehicle_type VARCHAR(30), created_at VARCHAR(30), updated_at VARCHAR(30))")
+
+        # self.connection.commit()
+        # cursor.close()
+        # print("Tables created successfully")
+        print("Holla Amigo")
+
+    def InsertOneTimeData(self, space_for_two, space_for_four):
+        cursor = self.connection.cursor()
         for x in range(space_for_two):
-            cursor.execute("INSERT into slots (space_for,is_empty) values ('2','1')")
-            self.mydb.commit()
+            cursor.execute("INSERT into slots (space_for, is_empty) values (2, 1)")
+            self.connection.commit()
 
         for x in range(space_for_four):
-            cursor.execute("INSERT into slots (space_for,is_empty) values ('4','1')")
-            self.mydb.commit()
+            cursor.execute("INSERT into slots (space_for, is_empty) values (4, 1)")
+            self.connection.commit()
         cursor.close()
 
-    def InsertAdmin(self,username,password):
-        cursor=self.mydb.cursor()
-        val=(username,password)
-        cursor.execute("INSERT into admin (username,password) values (%s,%s)",val)
-        self.mydb.commit()
+    def InsertAdmin(self, username, password):
+        cursor = self.connection.cursor()
+        val = (username, password)
+        cursor.execute("INSERT into admin (username, password) values (%s, %s)", val)
+        self.connection.commit()
         cursor.close()
 
-    def doAdminLogin(self,username,pasword):
-        cursor=self.mydb.cursor()
-        cursor.execute("select * from admin where username='"+username+"' and password='"+pasword+"'")
-        data=cursor.fetchall()
+    def doAdminLogin(self, username, password):
+        cursor = self.connection.cursor()
+        cursor.execute("SELECT * FROM admin WHERE username=%s AND password=%s", (username, password))
+        data = cursor.fetchall()
         cursor.close()
-        if len(data)>0:
+        if len(data) > 0:
             return True
         else:
             return False
 
     def getSlotSpace(self):
-        cursor=self.mydb.cursor()
-        cursor.execute("select * from slots")
-        data=cursor.fetchall()
+        cursor = self.connection.cursor()
+        cursor.execute("SELECT * FROM slots")
+        data = cursor.fetchall()
         cursor.close()
         return data
 
     def getCurrentVehicle(self):
-        cursor=self.mydb.cursor()
-        cursor.execute("select * from vehicles where is_exit='0'")
-        data=cursor.fetchall()
+        cursor = self.connection.cursor()
+        cursor.execute("SELECT * FROM vehicles WHERE is_exit='0'")
+        data = cursor.fetchall()
         cursor.close()
         return data
 
     def getAllVehicle(self):
-        cursor=self.mydb.cursor()
-        cursor.execute("select * from vehicles where is_exit='1'")
-        data=cursor.fetchall()
+        cursor = self.connection.cursor()
+        cursor.execute("SELECT * FROM vehicles WHERE is_exit='1'")
+        data = cursor.fetchall()
         cursor.close()
         return data
 
-    def AddVehicles(self,name,vehicleno,mobile,vehicle_type):
-        spacid=self.spaceAvailable(vehicle_type)
+    def AddVehicles(self, name, vehicle_no, mobile, vehicle_type):
+        spacid = self.spaceAvailable(vehicle_type)
         if spacid:
-            currentdata=datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-            data=(name,mobile,str(currentdata),'','0',vehicleno,str(currentdata),str(currentdata),vehicle_type)
-            cursor=self.mydb.cursor()
-            cursor.execute("INSERT into vehicles (name,mobile,entry_time,exit_time,is_exit,vehicle_no,created_at,updated_at,vehicle_type) values (%s,%s,%s,%s,%s,%s,%s,%s,%s)",data)
-            self.mydb.commit()
-            lastid=cursor.lastrowid
-            cursor.execute("UPDATE slots set vehicle_id='"+str(lastid)+"',is_empty='0' where id='"+str(spacid)+"'")
-            self.mydb.commit()
+            currentdata = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            data = (name, mobile, currentdata, '', '0', vehicle_no, currentdata, currentdata, vehicle_type)
+            cursor = self.connection.cursor()
+            cursor.execute("INSERT into vehicles (name, mobile, entry_time, exit_time, is_exit, vehicle_no, created_at, updated_at, vehicle_type) values (%s, %s, %s, %s, %s, %s, %s, %s, %s)", data)
+            self.connection.commit()
+            lastid = cursor.fetchone()[0]
+            cursor.execute("UPDATE slots SET vehicle_id=%s, is_empty=0 WHERE id=%s", (lastid, spacid))
+            self.connection.commit()
             cursor.close()
             return True
         else:
             return "No Space Available for Parking"
 
-
-    def spaceAvailable(self,v_type):
-        cursor=self.mydb.cursor()
-        cursor.execute("select * from slots where is_empty='1' and space_for='"+str(v_type)+"'")
-        data=cursor.fetchall()
+    def spaceAvailable(self, v_type):
+        cursor = self.connection.cursor()
+        cursor.execute("SELECT id FROM slots WHERE is_empty=1 AND space_for=%s", (v_type,))
+        data = cursor.fetchone()
         cursor.close()
-
-        if len(data)>0:
-            return data[0][0]
+        if data:
+            return data[0]
         else:
             return False
 
-    def exitVehicle(self,id):
-        cursor=self.mydb.cursor()
+    def exitVehicle(self, id):
+        cursor = self.connection.cursor()
         currentdata = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        cursor.execute("UPDATE slots set is_empty='1',vehicle_id='' where vehicle_id='"+id+"'")
-        self.mydb.commit()
-        cursor.execute("UPDATE vehicles set is_exit='1',exit_time='"+currentdata+"' where id='" + id + "'")
-        self.mydb.commit()
-
+        cursor.execute("UPDATE slots SET is_empty=1, vehicle_id=NULL WHERE vehicle_id=%s", (id,))
+        self.connection.commit()
+        cursor.execute("UPDATE vehicles SET is_exit='1', exit_time=%s WHERE id=%s", (currentdata, id))
+        self.connection.commit()
+        cursor.close()
